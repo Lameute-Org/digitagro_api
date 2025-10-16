163525115d67d3f569dbb0b1925da50e4e3fd41e
davyemane <davyemane2@gmail.com>
2025-10-16 21:33:39 +0100

    teste

--- apps/production/tests.py
+++ apps/production/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from apps.users.models import CustomUser
+from apps.production.models import Production
 
-# Create your tests here.
+class ProductionTestCase(TestCase):
+    def test_import(self):
+        """Test que les imports fonctionnent"""
+        self.assertTrue(True)
